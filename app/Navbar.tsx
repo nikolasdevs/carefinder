671c86737287a64db4d5e0f75cbbead499ece32f
Emmanuel Esan <nikolasdevs@gmail.com>
2024-08-08 22:48:27 +0100

    navbar style changed

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,12 +1,4 @@
-import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
-} from "@headlessui/react";
+import { Disclosure } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { onAuthStateChanged, signOut, User } from "firebase/auth";
 import { useRouter } from "next/navigation";
@@ -16,18 +8,23 @@ import { doc, getDoc } from "@firebase/firestore";
 import Image from "next/image";
 import logo from "../public/heroImg.png";
 import Link from "next/link";
-
-const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "About", href: "#", current: false },
-  { name: "Contact", href: "#", current: false },
-];
+import {
+  Menubar,
+  MenubarCheckboxItem,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import { LogOut, Menu, Settings, User as UserMenu } from "lucide-react";
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function Example() {
+export default function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
   const [user, setUser] = useState<User | null>(null);
   const router = useRouter();
 
@@ -55,115 +52,67 @@ export default function Example() {
       console.error("Logout Error:", error);
     }
   };
+
   return (
-    <Disclosure as="nav" className="bg-gray-800">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            {/* Mobile menu button*/}
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-              <span className="absolute -inset-0.5" />
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon
-                aria-hidden="true"
-                className="block h-6 w-6 group-data-[open]:hidden"
-              />
-              <XMarkIcon
-                aria-hidden="true"
-                className="hidden h-6 w-6 group-data-[open]:block"
-              />
-            </DisclosureButton>
+    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 shadow-on-scroll">
+      <div className="relative flex h-16 items-center justify-between">
+        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <Menubar>
+            <MenubarMenu>
+              <MenubarTrigger>
+                {" "}
+                <Menu />{" "}
+              </MenubarTrigger>
+              <MenubarContent>
+                <MenubarItem>Dashboard</MenubarItem>
+
+                <MenubarItem>About</MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+          </Menubar>
+        </div>
+        <Menubar className=" sm:flex justify-between hidden">
+          <div className=" flex">
+            <MenubarMenu>
+              <MenubarTrigger>Dashboard</MenubarTrigger>
+            </MenubarMenu>
+            <MenubarMenu>
+              <MenubarTrigger>About</MenubarTrigger>
+            </MenubarMenu>
           </div>
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <Link href='/dashboard' className="flex flex-shrink-0 items-center">
-              <Image alt="Company Logo" src={logo} className="h-8 w-auto" />
-            </Link>
-            <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    aria-current={item.current ? "page" : undefined}
-                    className={classNames(
-                      item.current
-                        ? "bg-gray-900 text-white"
-                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                      "rounded-md px-3 py-2 text-sm font-medium"
-                    )}
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-            </div>
-          </div>
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            {/* Profile dropdown */}
-            <Menu as="div" className="relative ml-3">
-              <div>
-                <MenuButton className="">
-                  <h1 className=" text-neutral-100">
-                    {user?.displayName} {user?.email}
-                  </h1>
-                </MenuButton>
-              </div>
-              <MenuItems
-                transition
-                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-              >
-                <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
-                  >
-                    Your Profile
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
-                  >
-                    Settings
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
-                  >
-                    <div className=" text-error mt-4" onClick={handleLogout}>
-                      Logout
-                    </div>
-                  </a>
-                </MenuItem>
-              </MenuItems>
-            </Menu>
-          </div>
+        </Menubar>
+
+        <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+          {/* Profile dropdown */}
+
+          <Menubar className="bg-transparent ">
+            <MenubarMenu>
+              <MenubarTrigger className=" cursor-pointer focus:bg-transparent focus:text-primary/70 hover:text-primary/70 data-[state=open]:text-primary/70 data-[state=open]:bg-transparent">
+                {user?.displayName} {user?.email}
+              </MenubarTrigger>
+              <MenubarContent>
+                <MenubarCheckboxItem>
+                  {" "}
+                  <UserMenu className="mr-2 h-4 w-4" />
+                  <span>Profile</span>
+                </MenubarCheckboxItem>
+                <MenubarCheckboxItem checked>
+                  <Settings className="mr-2 h-4 w-4" />
+                  <span>Settings</span>
+                </MenubarCheckboxItem>
+
+                <MenubarSeparator />
+
+                <MenubarItem onClick={handleLogout} inset>
+                  {" "}
+                  <LogOut className="mr-2 h-4 w-4" />
+                  <span>Log out</span>
+                </MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+          </Menubar>
         </div>
       </div>
-
-      <DisclosurePanel className="sm:hidden">
-        <div className="space-y-1 px-2 pb-3 pt-2">
-          {navigation.map((item) => (
-            <DisclosureButton
-              key={item.name}
-              as="a"
-              href={item.href}
-              aria-current={item.current ? "page" : undefined}
-              className={classNames(
-                item.current
-                  ? "bg-gray-900 text-white"
-                  : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                "block rounded-md px-3 py-2 text-base font-medium"
-              )}
-            >
-              {item.name}
-            </DisclosureButton>
-          ))}
-        </div>
-      </DisclosurePanel>
-    </Disclosure>
+    </div>
   );
 }
