d5308d5899dccfc818e4cff6c4aa3d7fb6ead13e
Emmanuel Esan <nikolasdevs@gmail.com>
2024-08-25 17:45:27 +0100

    password schema adjusted

--- utils/validationSchema.ts
+++ utils/validationSchema.ts
@@ -1,18 +1,25 @@
 // validationSchema.ts
 import { z } from "zod";
 
+export const passwordSchema = z
+  .string()
+  .min(6, "Password must be at least 6 characters")
+  .max(20, "Password cannot exceed 20 characters")
+  .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+  .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+  .regex(/\d/, "Password must contain at least one number")
+  .regex(/[@$!%*?&]/, "Password must contain at least one special character");
+
 export const loginSchema = z.object({
   email: z.string().email("Invalid email address"),
-  password: z.string().min(6, "Password must be at least 6 characters"),
+  password: passwordSchema,
 });
 
 export const registerSchema = z
   .object({
     email: z.string().email("Invalid email address"),
-    password: z.string().min(6, "Password must be at least 6 characters"),
-    confirmPassword: z
-      .string()
-      .min(6, "Password must be at least 6 characters"),
+    password: passwordSchema,
+    confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords do not match",
